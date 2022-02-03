151c35a50ab9a73f45f0888e1e3070504a46d844
sammy-code98 <sammysmart98@yahoo.com>
2022-02-03 18:45:52 +0100

    update ui

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -8,7 +8,7 @@
           class="shadow-lg border rounded px-3 py-2 w-full border-gray-500"
         />
         <button
-          class="bg-blue-600 text-white mt-6 p-2 rounded-md md:px-8 md:mt-0 md:p-0 md:mx-2"
+          class="bg-blue-600 shadow-lg text-white mt-6 p-2 rounded md:px-8 md:mt-0 md:p-0 md:mx-2"
         >
           Add Activity
         </button>
